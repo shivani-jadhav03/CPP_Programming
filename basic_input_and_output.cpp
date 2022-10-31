edf31390a4dd8c65f1290467429eddfe69ae7a56
Shivani Jadhav <jadhavshivani41003@gmail.com>
2022-10-31 12:25:20 +0530

    Added basic input and output

---
+++ basic_input_and_output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1;
+    int num2;
+
+    cout<<"Please enter num1 ";
+    cin>>num1;
+
+    cout<<"Please enter num2 ";
+    cin>>num2;
+
+    cout<<"Sum of num1 and num2 is "<<num1+num2<<endl;
+    return 0;
+}
