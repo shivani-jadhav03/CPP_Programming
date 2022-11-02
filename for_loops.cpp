a3feaf3face45940da16a497e9da4de013ca0778
Shivani Jadhav <jadhavshivani41003@gmail.com>
2022-11-02 22:26:28 +0530

    Added loop

---
+++ for_loops.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // For loop in C++ :
+for (int i=1; i<10; i++){       // for (initialization; condition; updation)
+    cout<<i<<endl;              // loop body (C++ code)
+}
+    return 0;
+}
---
+++ while_loop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main(){
+
+    // While loop in C++
+    int i=1;
+    while(i<=10){              // while (condition)
+        cout<<i<<endl;       // loop body (C++ code)
+        i++;
+    }
+    return 0;
+}
